1801c792d159f3112042abdbf1ae96bb8b5d6cfd
Wesley Bruno de Souza <wesleybruno.adm@gmail.com>
2023-11-10 19:42:50 -0300

    Validação do input celular finalizado

--- src/js/validadores_input/celular.js
+++ src/js/validadores_input/celular.js
@@ -1,26 +1,41 @@
 
 const regex = new RegExp('[0-9]{2}');
-const regex2 = new RegExp('[a-zA-Z]+');
-const celular = document.getElementById("celular");
+const regex2 = new RegExp('[!@#$%¨&*+-/a-zA-Z\s:;.,]+');
+const regex3 = new RegExp('\s+');
 
-
+const input = document.getElementById("cell_phone");
+const field = document.getElementById("field--cell_phone");
+const message = document.querySelector("#message.message--invalid");
 
 window.onload = function(){
     
-    celular.addEventListener("input", up);
+    input.addEventListener("input", up);
 
     function up() {
         
-        if(celular.value.length >= 2 && celular.value.length < 3) {
+        if(input.value.length >= 2 && input.value.length < 3) {
            
             try {
-                const valueNew = celular.value.replace(regex, `(${regex.exec(celular.value)[0]}) `);
-                celular.value = valueNew;
+                const valueNew = input.value.replace(regex, `(${regex.exec(input.value)[0]})`);
+                input.value = valueNew.trim();
                 
             } catch (err) {
                 console.log(err);
             };
         }
-        console.log(regex2.test(celular.value));
+
+        if(regex2.test(input.value)) {
+            field.classList.add('field--invalid');
+            input.classList.add('input--invalid');
+            message.style.display = 'inline-block';
+
+            input.value = input.value.trim();
+        } else {
+            field.classList.remove('field--invalid');
+            input.classList.remove('input--invalid');
+            message.style.display = 'none';
+
+            input.value = input.value.trim();
+        }
     }
 }
