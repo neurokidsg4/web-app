51c507fcaed2eac262c1da563a60afc70a91749b
Wesley Bruno de Souza <wesleybruno.adm@gmail.com>
2023-11-10 17:56:54 -0300

    Refatorando o codigo e trocando as nomenclaturas das classes e identificadores

--- src/js/validadores_input/celular.js
+++ src/js/validadores_input/celular.js
@@ -1,15 +1,26 @@
 
+const regex = new RegExp('[0-9]{2}');
+const regex2 = new RegExp('[a-zA-Z]+');
+const celular = document.getElementById("celular");
+
+
+
 window.onload = function(){
-    const regex = new RegExp('[0-9]{2}');
     
-    const celular = document.getElementById("celular");
-    celular.addEventListener("keyup", up);
+    celular.addEventListener("input", up);
 
     function up() {
         
         if(celular.value.length >= 2 && celular.value.length < 3) {
-            const valueNew = celular.value.replace(regex, `(${regex.exec(celular.value)[0]}) `);
-            celular.value = valueNew;
+           
+            try {
+                const valueNew = celular.value.replace(regex, `(${regex.exec(celular.value)[0]}) `);
+                celular.value = valueNew;
+                
+            } catch (err) {
+                console.log(err);
+            };
         }
+        console.log(regex2.test(celular.value));
     }
 }
