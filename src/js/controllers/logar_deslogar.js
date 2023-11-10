14416cf12f173528a9787e13ba9afcd2adab4607
Wesley Bruno de Souza <wesleybruno.adm@gmail.com>
2023-11-10 12:43:55 -0300

    Voltando as alterações ao que era antes

--- src/js/controllers/logar_deslogar.js
+++ src/js/controllers/logar_deslogar.js
@@ -6,10 +6,6 @@ const btnActionLogin = document.querySelectorAll('.btn_action_login');
 const btnLogout = document.querySelectorAll('.btn_logout');
 const btnLogin = document.querySelectorAll('.btn_login');
 
-const cabecalhoMenu = document.getElementById('cabecalho__menu');
-
-Database.insert('user', '{nome: qual}')
-
 if(Database.isLogged('user')) {
 
     btnLogin.forEach(btn => {
@@ -28,7 +24,6 @@ btnActionLogout.forEach(btn => {
 
         Database.delete('user');
         window.location.href = "./index.html";
-        cabecalhoMenu.style.display = 'none';
     })
 })
 
@@ -36,6 +31,5 @@ btnActionLogin.forEach(btn => {
     
     btn.addEventListener('click', () => {
         window.location.href = "./login.html";
-        cabecalhoMenu.style.display = 'none';
     })
 })
