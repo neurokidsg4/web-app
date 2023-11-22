520294f2b6bc90b7f09705301e7a2f8e9d67941d
Gustavo da Silva Novaes <gustavosilvanovaes@outlook.com>
2023-11-22 20:52:45 -0300

    jogo mat

--- src/jogo-matematica.js
+++ src/jogo-matematica.js
@@ -1,18 +1,29 @@
-var num1 = parseFloat(document.getElementById("num1").innerHTML);
-var num2 = parseFloat(document.getElementById("num2").innerHTML);
+
+var num1 = parseInt(Math.random() * 10);
+var num2 = parseInt(Math.random() * 10);
+
+document.getElementById("num1").innerHTML = num1;
+document.getElementById("num2").innerHTML = num2;
+
 
 btnConfimar = document.getElementById("iconfirm");
 
-btnConfimar.addEventListener("click", function(){
-    
+btnConfimar.addEventListener("click", function () {
+
+
     var calculo = num1 + num2;
 
     var resposta = document.getElementById("resposta").value;
 
-    if(calculo == resposta)
-    document.getElementById("resultado").innerHTML = "Parabéns! Você acertou.";
+    if (calculo == resposta) {
+        document.getElementById("resultado").innerHTML = `Você acertou`;
+        location.reload();
+
+    }
 
     else
-    document.getElementById("resultado").innerHTML = "Tente mais uma vez.";
+        document.getElementById("resultado").innerHTML = "Tente mais uma vez.";
+
 });
+
 
