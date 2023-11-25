2bcd8ced6b86901560f0a33c61768d5575b10fe4
Gustavo da Silva Novaes <gustavosilvanovaes@outlook.com>
2023-11-25 19:05:34 -0300

    jogo matematica

--- src/jogo-matematica.js
+++ src/jogo-matematica.js
@@ -1,29 +1,67 @@
 
-var num1 = parseInt(Math.random() * 10);
-var num2 = parseInt(Math.random() * 10);
+var num1 = 0;
+var num2 = 0;
+var cont = 1;
 
-document.getElementById("num1").innerHTML = num1;
-document.getElementById("num2").innerHTML = num2;
+const btnContinue = document.getElementById("icontinue");
+btnContinue.style.display = "none";
 
+//Gera dois número aleatório de 0 a 10.
+function geraNum() {
 
+    num1 = parseInt(Math.random() * 10);
+    num2 = parseInt(Math.random() * 10);
+
+    document.getElementById("num1").innerHTML = num1;
+    document.getElementById("num2").innerHTML = num2;
+
+}
+
+geraNum();
+
+//Ação ao confirmar a resposta.
 btnConfimar = document.getElementById("iconfirm");
 
 btnConfimar.addEventListener("click", function () {
 
-
     var calculo = num1 + num2;
-
     var resposta = document.getElementById("resposta").value;
 
+
     if (calculo == resposta) {
-        document.getElementById("resultado").innerHTML = `Você acertou`;
-        location.reload();
+
+        document.getElementById("pontos").innerHTML = `Acertos: ${cont}`;
+        document.getElementById("resultado").innerHTML = `Parabéns! Você acertou`;
+
+        if (cont < 5) {
+
+            btnContinue.style.display = "block";
+            btnContinue.addEventListener("click", function () {
+
+                geraNum();
+                document.getElementById("resposta").value = "";
+                document.getElementById("resultado").innerHTML = "";
+                btnContinue.style.display = "none";
+            });
+
+
+            cont++;
+
+
+        }
+
+        else
+            document.getElementById("resultado").innerHTML = "Próxima fase";
 
     }
 
-    else
+    else{
+
         document.getElementById("resultado").innerHTML = "Tente mais uma vez.";
+        document.getElementById("resposta").value = "";
+
+    }
+
+
 
 });
-
-
