72b73e3792f7a4fb6f78aa2b6d3af006ec9e53be
Pedro Rodrigues <pedrolrodrigues3@gmail.com>
2023-11-27 11:20:35 -0300

    finalizando api cep

--- src/busca_cep.js
+++ src/busca_cep.js
@@ -1,13 +1,13 @@
 document.getElementById('cep').addEventListener('blur', function () {
-    // Obtenha o valor do CEP
+    // Obtem o valor do CEP
     var cep = document.getElementById('cep').value;
 
     // Consulte a API do ViaCEP
     fetch('https://viacep.com.br/ws/' + cep + '/json/')
         .then(response => response.json())
         .then(data => {
-            // Preencha automaticamente os campos de endereço, cidade e estado
-            document.getElementById('endereco').value = data.logradouro;
+            // Preenche automaticamente os campos de endereço, cidade e estado
+            // document.getElementById('endereco').value = data.logradouro;
             document.getElementById('cidade').value = data.localidade;
             document.getElementById('estado').value = data.uf;
         })
