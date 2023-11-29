e4010267350481873a27fbb615cf85da234235f5
Wesley Bruno de Souza <wesleybruno.adm@gmail.com>
2023-11-28 22:12:39 -0300

    perfil

--- src/perfil.js
+++ src/perfil.js
@@ -1,51 +1,88 @@
 /*Pegando os dados atuais do perfil e salvando no localstorage*/
+import { LocalStorage } from './js/services/LocalStorage.js';
 
-btnEditar = document.getElementById('btn-edit');
+const btnEditar = document.getElementById('btn-edit');
+const user = LocalStorage.select('user');
+const id = user[0].id;
+const url = `http://localhost:3000/users/${id}`;
 
-btnEditar.addEventListener("click", function(){
+fetch(url, {
+    method : "GET",
+    headers: {'Content-Type': 'application/json'}
+})
+.then(response => response.json())
+.then((user) => {
 
-    var nome = document.getElementById('info-name-1').innerHTML;
-    localStorage.setItem("nome", nome);
+    console.log(user);
+    var novoNome = user.name;
+    document.getElementById("info-name-1").innerHTML = novoNome;
+    
+    var novoGenero = user.gender;
+    document.getElementById("info-gender-1").innerHTML = novoGenero;
 
-    var genero = document.getElementById('info-gender-1').innerHTML;
-    localStorage.setItem("genero", genero);
+    var novoNascimento = localStorage.getItem("nascimento");
+    document.getElementById("info-birth-1").innerHTML = novoNascimento;
 
-    var nascimento = document.getElementById('info-birth-1').innerHTML;
-    localStorage.setItem("nascimento", nascimento);
+    var novoEmail = localStorage.getItem("email");
+    document.getElementById("info-email-1").innerHTML = novoEmail;
 
-    var email = document.getElementById('info-email-1').innerHTML;
-    localStorage.setItem("email", email);
+    var novoCell = localStorage.getItem("cell");
+    document.getElementById("info-cell-1").innerHTML = novoCell;
 
-    var cell = document.getElementById("info-cell-1").innerHTML;
-    localStorage.setItem("cell", cell);
+    var novoEndereco = localStorage.getItem("endereco");
+    document.getElementById("info-add-1").innerHTML = novoEndereco;
 
-    var endereco = document.getElementById("info-add-1").innerHTML;
-    localStorage.setItem("endereco", endereco);
+    var novoSobreMim = localStorage.getItem("sobreMim");
+    document.getElementById("iabout-me").innerHTML = novoSobreMim;
 
-    var sobreMim = document.getElementById("iabout-me").innerHTML;
-    localStorage.setItem("sobreMim", sobreMim);
+})
+.catch(err => {});
 
-});
 
-/*Pegando os novos dados do localstorage e colocando no perfil */
+// btnEditar.addEventListener("click", function(){
 
-var novoNome = localStorage.getItem("nome");
-document.getElementById("info-name-1").innerHTML = novoNome;
+//     var nome = document.getElementById('info-name-1').innerHTML;
+//     localStorage.setItem("nome", nome);
+
+//     var genero = document.getElementById('info-gender-1').innerHTML;
+//     localStorage.setItem("genero", genero);
+
+//     var nascimento = document.getElementById('info-birth-1').innerHTML;
+//     localStorage.setItem("nascimento", nascimento);
+
+//     var email = document.getElementById('info-email-1').innerHTML;
+//     localStorage.setItem("email", email);
+
+//     var cell = document.getElementById("info-cell-1").innerHTML;
+//     localStorage.setItem("cell", cell);
+
+//     var endereco = document.getElementById("info-add-1").innerHTML;
+//     localStorage.setItem("endereco", endereco);
+
+//     var sobreMim = document.getElementById("iabout-me").innerHTML;
+//     localStorage.setItem("sobreMim", sobreMim);
+
+// });
+
+// /*Pegando os novos dados do localstorage e colocando no perfil */
+
+// var novoNome = localStorage.getItem("nome");
+// document.getElementById("info-name-1").innerHTML = novoNome;
   
-var novoGenero = localStorage.getItem("genero");
-document.getElementById("info-gender-1").innerHTML = novoGenero;
+// var novoGenero = localStorage.getItem("genero");
+// document.getElementById("info-gender-1").innerHTML = novoGenero;
 
-var novoNascimento = localStorage.getItem("nascimento");
-document.getElementById("info-birth-1").innerHTML = novoNascimento;
+// var novoNascimento = localStorage.getItem("nascimento");
+// document.getElementById("info-birth-1").innerHTML = novoNascimento;
 
-var novoEmail = localStorage.getItem("email");
-document.getElementById("info-email-1").innerHTML = novoEmail;
+// var novoEmail = localStorage.getItem("email");
+// document.getElementById("info-email-1").innerHTML = novoEmail;
 
-var novoCell = localStorage.getItem("cell");
-document.getElementById("info-cell-1").innerHTML = novoCell;
+// var novoCell = localStorage.getItem("cell");
+// document.getElementById("info-cell-1").innerHTML = novoCell;
 
-var novoEndereco = localStorage.getItem("endereco");
-document.getElementById("info-add-1").innerHTML = novoEndereco;
+// var novoEndereco = localStorage.getItem("endereco");
+// document.getElementById("info-add-1").innerHTML = novoEndereco;
 
-var novoSobreMim = localStorage.getItem("sobreMim");
-document.getElementById("iabout-me").innerHTML = novoSobreMim;
+// var novoSobreMim = localStorage.getItem("sobreMim");
+// document.getElementById("iabout-me").innerHTML = novoSobreMim;
