668aee3584b772243c95bdb44ed5c0a936314c9e
Pedro Rodrigues <pedrolrodrigues3@gmail.com>
2023-12-05 09:05:50 -0300

    inclusão de acumulador tentativas e contador acertos com armazenamento no localStorage jogo de cores

--- src/atividades_cores_script.js
+++ src/atividades_cores_script.js
@@ -5,12 +5,28 @@ const message = document.querySelector('#message');
 const continueButton = document.querySelector('#continue');
 const backButton = document.querySelector('#back');
 let correctColorIndex = 2; // Índice do círculo correto (0-5)
+let numAttempts = 0;
+let numCorrect = 0;
 
 // Array de cores para o jogo
 const colors = ['red', 'green', 'blue', 'yellow', 'purple', 'orange'];
 
 // Função para iniciar o jogo
 function startGame() {
+    // Incrementar o número de tentativas
+    numAttempts++;
+
+    // Verificar se atingiu o limite de tentativas
+    if (numAttempts > 5) {
+        // Limpar a mensagem e exibir apenas o botão de voltar
+        message.textContent = '';
+        continueButton.style.display = 'none';
+        backButton.style.display = 'block';
+        backButton.addEventListener('click', handleBackButtonClick);
+        saveNumCorrect();
+        return;
+    }
+
     // Escolher uma cor aleatória para adivinhar
     correctColorIndex = Math.floor(Math.random() * colors.length);
 
@@ -43,30 +59,34 @@ function handleCircleClick(event) {
     
     if (clickedColorIndex === correctColorIndex) {
         // O jogador acertou
-        message.textContent = 'Você acertou! Parabéns!';
-        continueButton.style.display = 'block';
-        continueButton.addEventListener('click', startGame);
-        backButton.style.display = 'block';
-        backButton.addEventListener('click', handleBackButtonClick);
+        numCorrect++;
+        message.textContent = 'Você acertou! Parabéns!\n' + numCorrect + ' de 5';
     } else {
         // O jogador errou
         message.textContent = 'Você errou. Tente novamente.';
-        backButton.style.display = 'block';
-        backButton.addEventListener('click', handleBackButtonClick);
-        continueButton.style.display = 'block';
-        continueButton.addEventListener('click', startGame);
     }
-    
+
     // Remover os ouvintes de clique dos círculos
     for (const circle of circles) {
         circle.removeEventListener('click', handleCircleClick);
     }
+
+    // Exibir o botão de continuar
+    continueButton.style.display = 'block';
+    continueButton.addEventListener('click', startGame);
+    backButton.style.display = 'block';
+    backButton.addEventListener('click', handleBackButtonClick);
 }
 
 // Função para lidar com o clique no botão "Voltar"
 function handleBackButtonClick() {
     // Redireciona para a página desejada
     window.location.href = '/src/area_infantil.html';
+}
+
+// Função para salvar o número de acertos no localStorage
+function saveNumCorrect() {
+    localStorage.setItem('acertos cores', numCorrect);
 }
 
 // Iniciar o jogo quando a página carregar
