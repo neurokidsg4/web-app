ff45312b605f982677032b49afe9acd4587dccaa
Gustavo da Silva Novaes <gustavosilvanovaes@outlook.com>
2023-12-07 20:56:24 -0300

    Update perfil.js

--- src/perfil.js
+++ src/perfil.js
@@ -2,6 +2,7 @@
 const usuario = localStorage.getItem('usuario') ? JSON.parse(localStorage.getItem('usuario')) : {};
 
 document.getElementById("info-name-1").innerHTML = usuario.nome;
+document.getElementById("name").innerHTML = usuario.nome;
 document.getElementById("info-gender-1").innerHTML = usuario.genero;
 document.getElementById("info-birth-1").innerHTML = usuario.nascimento;
 document.getElementById("info-email-1").innerHTML = usuario.email;
@@ -10,3 +11,5 @@ document.getElementById("info-add-1").innerHTML = usuario.cep;
 document.getElementById("iabout-me").innerHTML = usuario.sobreMin;
 document.getElementById("info-add-estado").innerHTML = usuario.estado;
 document.getElementById("info-add-cidade").innerHTML = usuario.cidade;
+document.getElementById("ocupation").innerHTML = usuario.atuacao;
+
