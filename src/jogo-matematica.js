036f41d302e5a8725bb3ec456f3d22ac59c0c78c
Gustavo da Silva Novaes <gustavosilvanovaes@outlook.com>
2023-12-07 00:08:23 -0300

    jogo matematica

--- src/jogo-matematica.js
+++ src/jogo-matematica.js
@@ -1,7 +1,12 @@
 
+//Se comentar as 4 linha enumeradas abaixo o jogo roda normalmente.
+
+//comentar essa linha (1)
 import { Game } from './js/entities/Jogo.js';
 
+//comentar essa linha(2)
 const jogo = new Game('Matematica');
+
 
 var num1 = 0;
 var num2 = 0;
@@ -43,6 +48,7 @@ btnConfimar.addEventListener("click", function () {
 
     if (calculo == resposta) {
 
+        //comentar essa linha(3)
         jogo.sumPoint();
 
         document.getElementById("pontos").innerHTML = `Acertos: ${cont}`;
@@ -50,6 +56,7 @@ btnConfimar.addEventListener("click", function () {
 
         if (cont < 5) {
 
+            //cometar essa linha(4)
             jogo.attempt();
             
             btnContinue.style.display = "block";
