b753716ec17a309a564669777e58aadccf6fb67f
Martha Beatriz <marthinhabeatriz@gmail.com>
2023-12-09 09:12:30 -0300

    Subida da correção do jogo de contagem

--- src/jogo_contagem_objetos.js
+++ src/jogo_contagem_objetos.js
@@ -1,117 +1,146 @@
 function getRandomColor() {
-    const colors = ['green', 'yellow', 'red', 'blue'];
-    const randomIndex = Math.floor(Math.random() * colors.length);
-    return colors[randomIndex];
-  }
-  
-  const icons = document.querySelectorAll('.objects_description img');
-  const colorTextElement = document.querySelector('.color_text');
-  const buttons = document.querySelectorAll('.button');
-  const messageElement = document.getElementById('message');
-  const backButton = document.getElementById('back');
-  const continueButton = document.getElementById('continue');
-  const buttonMessageContainer = document.querySelector('.button-message-container');
-  
-  const maxAttempts = 3;
-  let currentAttempt = 0;
-  
-  function updateColors() {
-    currentAttempt++;
-  
-    if (currentAttempt > maxAttempts) {
-      messageElement.textContent = `Fim de jogo. Você tentou ${maxAttempts} vezes. Reiniciando...`;
-      setTimeout(() => {
-        currentAttempt = 0;
-        updateColors();
-      }, 2000);
-      return;
-    }
-  
-    let randomColor;
-    let coloredObjects;
-    let correctNumber;
-  
-    do {
-      randomColor = getRandomColor();
-      coloredObjects = Array.from(icons).filter(icon => {
-        const iconColor = getRandomColor();
-        icon.style.backgroundColor = iconColor;
-        return iconColor === randomColor;
-      });
-  
-      correctNumber = coloredObjects.length;
-    } while (coloredObjects.length === 0);
-  
-    const textByColor = {
-      'green': 'Verde',
-      'yellow': 'Amarelo',
-      'red': 'Vermelho',
-      'blue': 'Azul',
-    };
-  
-    colorTextElement.style.color = randomColor;
-    colorTextElement.textContent = textByColor[randomColor];
-  
-    const availableNumbers = [1, 2, 3, 4, 5];
-  
-    const shuffledNumbers = availableNumbers.sort(() => Math.random() - 0.5);
-  
-    buttons.forEach((button, index) => {
-      const buttonText = index + 1 === correctNumber ? correctNumber : shuffledNumbers.pop();
-      button.textContent = buttonText;
-    });
+  const colors = ['green', 'yellow', 'blue', 'red'];
+  const randomIndex = Math.floor(Math.random() * colors.length);
+  return colors[randomIndex];
+}
 
-    icons.forEach(icon => {
-        const iconColor = getRandomColor();
-        icon.style.backgroundColor = iconColor;
-        icon.classList.add('rounded');
-      });
-  
-    messageElement.textContent = '';
+const icons = document.querySelectorAll('.objects_description img');
+const colorTextElement = document.querySelector('.color_text');
+const buttons = document.querySelectorAll('.button');
+const messageElement = document.getElementById('message');
+const backButton = document.getElementById('back');
+const continueButton = document.getElementById('continue');
+const buttonMessageContainer = document.querySelector('.button-message-container');
+
+
+const maxRounds = 3;
+let currentRound = 0;
+
+
+function updateColors() {
+  currentRound++;
+
+  // Mapeia os objetos da página com suas cores
+  const objectsWithColors = Array.from(icons).map(icon => {
+    const iconColor = getRandomColor();
+    icon.style.backgroundColor = iconColor; // Atualiza a cor de fundo do objeto
+    return { icon, color: iconColor };
+  });
+
+  // Seleciona aleatoriamente um objeto
+  const randomIndex = Math.floor(Math.random() * objectsWithColors.length);
+  const targetObject = objectsWithColors[randomIndex];
+
+  // Define a cor e atualiza a interface
+  const targetColor = targetObject.color;
+  colorTextElement.style.color = targetColor;
+  colorTextElement.textContent = targetColor.charAt(0).toUpperCase() + targetColor.slice(1);
+
+  // Conta a quantidade de objetos com a cor
+  const countTargetColor = objectsWithColors.filter(obj => obj.color === targetColor).length;
+
+  // Gera uma lista com números diferentes da quantidade correta
+  const otherNumbers = generateUniqueNumbers(countTargetColor, buttons.length);
+
+  // Escolhe aleatoriamente um índice para o botão correto
+  const correctButtonIndex = Math.floor(Math.random() * buttons.length);
+  otherNumbers[correctButtonIndex] = countTargetColor;
+
+   buttons.forEach((button, index) => {
+    button.textContent = otherNumbers[index];
+    button.removeEventListener('click', handleButtonClick);
+    button.addEventListener('click', function () {
+      handleButtonClick.call(this, countTargetColor);
+    });
+  });
+
+  // Limpa a mensagem e exibe os botões
+  messageElement.textContent = '';
+  showButtons();  
+}
+
+continueButton.addEventListener('click', handleContinueClick);
+
+function generateUniqueNumbers(exclude, count) {
+  const uniqueNumbers = [];
+
+  while (uniqueNumbers.length < count) {
+    const number = getRandomNumber(exclude);
+    if (!uniqueNumbers.includes(number)) {
+      uniqueNumbers.push(number);
+    }
+  }
+
+  return uniqueNumbers;
+}
+
+function getRandomNumber(exclude) {
+  let number;
+  do {
+    number = Math.floor(Math.random() * 6) + 1;
+  } while (number === exclude);
+  return number;
+}
+
+function showButtons() {
+  buttonMessageContainer.style.display = 'flex';
+}
+
+function hideButtons() {
+  buttonMessageContainer.style.display = 'none';
+}
+
+function handleButtonClick(countTargetColor) {
+  const selectedText = this.textContent.trim();
+
+  // Verifica se o texto não está vazio
+  if (!selectedText) {
+    console.error('Erro: O texto está vazio.');
+    return;
+  }
+
+  // Converte o texto para número
+  const selectedNumber = parseInt(selectedText, 10);
+
+  if (isNaN(selectedNumber)) {
+    console.error(`Erro: Não foi possível converter o texto "${selectedText}" para número.`);
+    return;
+  }
+
+  console.log(`Número selecionado: ${selectedNumber}`);
+  console.log(`Contagem alvo: ${countTargetColor}`);
+
+  // Verifica se o número selecionado é o mesmo da cor alvo
+  if (selectedNumber === countTargetColor) {
+    messageElement.textContent = 'Parabéns! Você acertou!';
+    showButtons(); 
+  } else {
+    messageElement.textContent = 'Ops! Tente novamente.';
     showButtons();
   }
-  
-  function showButtons() {
-    buttonMessageContainer.style.display = 'flex';
-    continueButton.style.display = 'block';
-  }
-  
-  function hideButtons() {
-    buttonMessageContainer.style.display = 'none';
-    continueButton.style.display = 'none';
-  }
-  
 
-  backButton.addEventListener('click', function () {
-    
-    window.location.href = './area_infantil.html';
-  });
+  currentAttempt++;
+  totalAttempts++;
+
+  if (currentRound <= maxRounds) {
+    // Exibe mensagem de sucesso ou falha
+    messageElement.textContent = (selectedNumber === countTargetColor) ? 'Parabéns! Você acertou!' : 'Ops! Tente novamente.';
+    showButtons();
+  } else {
   
-  continueButton.addEventListener('click', function () {
-    hideButtons();
-    
-  });
-  
-  buttons.forEach(button => {
-    button.addEventListener('click', function () {
-      const selectedNumber = parseInt(this.textContent);
-      const coloredObjects = Array.from(icons).filter(icon => icon.style.backgroundColor === colorTextElement.style.color);
-  
-      if (selectedNumber === coloredObjects.length) {
-        messageElement.textContent = 'Parabéns! Você acertou!';
-       
-        showButtons();
-      } else {
-        messageElement.textContent = 'Ops! Tente novamente.';
-        
-        showButtons();
-      }
-  
-      // Chama a função para definir as cores após um curto intervalo
-      setTimeout(updateColors, 1000);
-    });
-  });
-  
+    messageElement.textContent = `Fim de jogo. Você tentou ${totalAttempts} vezes em ${maxRounds} rodadas. Limite de rodadas atingido.`;
+    showButtons();
+  }
+}
+
+function handleContinueClick() {
+  // Reinicia o número de tentativas, a contagem total de tentativas e as rodadas
+  totalAttempts = 0;
+  roundsCompleted = 0;
+
   hideButtons();
   updateColors();
-  
+}
+
+hideButtons();
+updateColors();
