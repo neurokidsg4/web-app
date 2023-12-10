71ce1616fa18f08df11e11d011da5be73caac83c
Martha Beatriz <marthinhabeatriz@gmail.com>
2023-12-10 11:25:01 -0300

    Subida do jogo contagem de objetos contendo todas as correções necessárias.

--- src/jogo_contagem_objetos.js
+++ src/jogo_contagem_objetos.js
@@ -4,6 +4,13 @@ function getRandomColor() {
   return colors[randomIndex];
 }
 
+const colorsMapping = {
+  'green': 'Verde',
+  'yellow': 'Amarelo',
+  'blue': 'Azul',
+  'red': 'Vermelho'
+};
+
 const icons = document.querySelectorAll('.objects_description img');
 const colorTextElement = document.querySelector('.color_text');
 const buttons = document.querySelectorAll('.button');
@@ -12,10 +19,9 @@ const backButton = document.getElementById('back');
 const continueButton = document.getElementById('continue');
 const buttonMessageContainer = document.querySelector('.button-message-container');
 
-
-const maxRounds = 3;
+const maxAttemptsPerRound = 3;
 let currentRound = 0;
-
+let totalAttempts = 0;
 
 function updateColors() {
   currentRound++;
@@ -23,7 +29,7 @@ function updateColors() {
   // Mapeia os objetos da página com suas cores
   const objectsWithColors = Array.from(icons).map(icon => {
     const iconColor = getRandomColor();
-    icon.style.backgroundColor = iconColor; // Atualiza a cor de fundo do objeto
+    icon.style.backgroundColor = iconColor;
     return { icon, color: iconColor };
   });
 
@@ -34,12 +40,12 @@ function updateColors() {
   // Define a cor e atualiza a interface
   const targetColor = targetObject.color;
   colorTextElement.style.color = targetColor;
-  colorTextElement.textContent = targetColor.charAt(0).toUpperCase() + targetColor.slice(1);
+  colorTextElement.textContent = colorsMapping[targetColor] || targetColor;
 
   // Conta a quantidade de objetos com a cor
   const countTargetColor = objectsWithColors.filter(obj => obj.color === targetColor).length;
 
-  // Gera uma lista com números diferentes da quantidade correta
+  // Gera uma lista com números diferentes da quantidade correta (evitar o erro de duplicidade)
   const otherNumbers = generateUniqueNumbers(countTargetColor, buttons.length);
 
   // Escolhe aleatoriamente um índice para o botão correto
@@ -47,7 +53,7 @@ function updateColors() {
   otherNumbers[correctButtonIndex] = countTargetColor;
 
    buttons.forEach((button, index) => {
-    button.textContent = otherNumbers[index];
+    button.textContent = colorsMapping[otherNumbers[index]] || otherNumbers[index];
     button.removeEventListener('click', handleButtonClick);
     button.addEventListener('click', function () {
       handleButtonClick.call(this, countTargetColor);
@@ -91,6 +97,7 @@ function hideButtons() {
 }
 
 function handleButtonClick(countTargetColor) {
+  // Captura o texto fora do bloco if
   const selectedText = this.textContent.trim();
 
   // Verifica se o texto não está vazio
@@ -110,36 +117,37 @@ function handleButtonClick(countTargetColor) {
   console.log(`Número selecionado: ${selectedNumber}`);
   console.log(`Contagem alvo: ${countTargetColor}`);
 
-  // Verifica se o número selecionado é o mesmo da cor alvo
+  // Verifica se o número selecionado é o mesmo da cor escolhida
   if (selectedNumber === countTargetColor) {
     messageElement.textContent = 'Parabéns! Você acertou!';
-    showButtons(); 
+    showButtons();
   } else {
     messageElement.textContent = 'Ops! Tente novamente.';
     showButtons();
   }
 
-  currentAttempt++;
   totalAttempts++;
 
-  if (currentRound <= maxRounds) {
-    // Exibe mensagem de sucesso ou falha
-    messageElement.textContent = (selectedNumber === countTargetColor) ? 'Parabéns! Você acertou!' : 'Ops! Tente novamente.';
+  if (totalAttempts > maxAttemptsPerRound) {
+    messageElement.textContent = 'Você atingiu o limite de tentativas. Deseja continuar?';
     showButtons();
-  } else {
-  
-    messageElement.textContent = `Fim de jogo. Você tentou ${totalAttempts} vezes em ${maxRounds} rodadas. Limite de rodadas atingido.`;
-    showButtons();
+    
+    // Reinicia o número de tentativas para a próxima rodada
+    totalAttempts = 0;
   }
 }
-
 function handleContinueClick() {
-  // Reinicia o número de tentativas, a contagem total de tentativas e as rodadas
-  totalAttempts = 0;
-  roundsCompleted = 0;
+  // Incrementa para a próxima rodada
+  currentRound++;
 
+  // Reinicia o número de tentativas e oculta os botões
+  totalAttempts = 0;
   hideButtons();
+
+  // Atualiza as cores para a próxima rodada
   updateColors();
+
+  messageElement.textContent = '';
 }
 
 hideButtons();
